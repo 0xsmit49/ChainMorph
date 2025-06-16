8ad18473c157063f60c5951ff77f9c8167242280
Smitbafna <bafnasmit@gmail.com>
2025-06-16 10:00:00 +0530

    game token added

--- contracts/src/CrossChainBridge.sol
+++ contracts/src/CrossChainBridge.sol
@@ -10,29 +10,7 @@ import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/FunctionsClient.sol";
 
-// ================================
-// 1. GAME TOKEN (ERC20)
-// ================================
-contract GameToken is ERC20, AccessControl {
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
 
-    constructor() ERC20("GameFi Token", "GAME") {
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _grantRole(MINTER_ROLE, msg.sender);
-        
-        // Genesis mint
-        _mint(msg.sender, 1000000 * 10**18);
-    }
-
-    function mint(address to, uint256 amount) external onlyRole(MINTER_ROLE) {
-        _mint(to, amount);
-    }
-
-    function burn(address from, uint256 amount) external onlyRole(BURNER_ROLE) {
-        _burn(from, amount);
-    }
-}
 
 // ================================
 // 2. TRAIT FUSION PRECOMPILE INTERFACE
